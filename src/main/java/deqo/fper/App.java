c8c0b49cb3089450671d5c0250c12a99d2a60ef6
PERU <florian.peru@gmail.com>
2018-10-22 17:37:54 +0200

    first commit

---
+++ src/main/java/deqo/fper/App.java
@@ -0,0 +1,13 @@
+package deqo.fper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
